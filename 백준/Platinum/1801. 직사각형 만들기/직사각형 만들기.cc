28eb24be83742636f4cf201d114b864317dc8671
GaeunLee <62925109+0923ule@users.noreply.github.com>
2024-06-10 01:16:39 +0900

    [Platinum IV] Title: 직사각형 만들기, Time: 484 ms, Memory: 99676 KB -BaekjoonHub

--- 백준/Platinum/1801. 직사각형 만들기/직사각형 만들기.cc
+++ 백준/Platinum/1801. 직사각형 만들기/직사각형 만들기.cc
@@ -1,78 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <bitset>
 #include <algorithm>
 using namespace std;
-typedef long long ll;
 
 int n;
+int sum = 0;
 vector<int> v;
-int longStick[1<<16] = {0,}; // longStick[state] 상태가 state일 때 가장 긴 스틱의 길이(2)
+
+bool dp[100][100][100][100] = {0,};
 
 void calc() {
-    for (int i = 1; i < (1<<n); i++) {
-		vector<int> subset;
-		int sum  = 0;
+	dp[0][0][0][0] = 1;
+	for (int i = 0; i < n; i++) {
 
-		// 부분수열의 집합 구하기
-		for (int j = 0; j < n; j++) {
-			if (i & (1<<j)) {
-				subset.push_back(v[j]);
-				sum += v[j];
-			}
-		}
-		if (sum % 2 == 1) continue;
-
-		// 부분수열의 부분수열 구하기
-		// 길이가 sum/2가 되는 부분수열이 있다 ==> sum/2 길이의 2개의 막대를 만들 수 있음
-		int flag = 0;
-		for (int a = 1; a < (1<<subset.size()); a++) {
-			int subsum = 0;
-			for (int b = 0; b < subset.size(); b++) {
-				if (a & (1<<b)) subsum += subset[b];
-			}
-			if (subsum == sum/2) {
-				flag = 1;
-				break;
-			}
-		}
-
-		if (flag) longStick[i] = sum/2;
-	}
-
-	// longStick 배열 채우기
-	for (int i = 0; i < (1<<n); i++) {
-		for (int j = 0; j < n; j++) {
-			// 현재 상태가 j막대를 포함하면, j막대가 없을 때랑 비교
-			if ((i>>j) & 1) {
-				longStick[i] = max(longStick[i], longStick[i ^ (1<<j)]);
+		// dp 채우기
+		for (int a = 80-v[i]; a >= 0; a--) {
+			for (int b = 80-v[i]; b >= 0; b--) {
+				for (int c = 80-v[i]; c >= 0; c--) {
+					for (int d = 80-v[i]; d >= 0; d--) {
+						if (dp[a][b][c][d]) {
+							dp[a+v[i]][b][c][d] = 1;
+							dp[a][b+v[i]][c][d] = 1;
+							dp[a][b][c+v[i]][d] = 1;
+							dp[a][b][c][d+v[i]] = 1;
+						}
+					}
+				}
 			}
 		}
 	}
 }
 
-int main() {
 
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   
 	cin >> n;
+
 	for (int i = 0; i < n; i++) {
 		int tmp;
 		cin >> tmp;
 		v.push_back(tmp);
+		sum += tmp;
 	}
-
 	calc();
 
+	// v[0] <= width <= height
+	// width <= sum / 4, width + height <= sum / 2
 	int maxi = -1;
-	int full_bits = (1<<n)-1;
-
-	for (int state = 0; state < (1<<n); state++) {
-		int area = longStick[state] * longStick[full_bits ^ state];
-		maxi = max(maxi, area);
+	for (int w = v[0]; w <= 80; w++) {
+		for (int h = w; h <= 80 ; h++) {
+			if (dp[w][w][h][h]) maxi = max(maxi, w * h);
+		}
 	}
 
-	if (maxi == 0) cout << -1;
-	else cout << maxi;
-
+	cout << maxi;
 	return 0;
 }
